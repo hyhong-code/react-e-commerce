37254cdb59e055bca29a43974367e8ec44bdf969
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-06 13:47:00 -0700

    refactor homepage into components and finish styling

---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomepageStyles.scss";
+import Directory from "../components/Directory";
+
+function Homepage(props) {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default Homepage;
